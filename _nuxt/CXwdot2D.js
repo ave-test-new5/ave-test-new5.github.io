a5575560c7223cc90a24f0ccb00512695874bda5
amy8023 <2847361548@qq.com>
2026-09-17 09:32:58 +0800

    deploy

---
+++ _nuxt/CXwdot2D.js
@@ -0,0 +1,1 @@
+import{Ki as t}from"./DK4YNJ99.js";import{t as r}from"./CjXFBzx4.js";function o(o,e){return o&&"string"==typeof o?/^(?:https?:)?\/\/|^data:|^blob:/i.test(o)?o:`${(e||function(){try{return t().globalConfig?.token_logo_url||"https://www.iconaves.com/"}catch{return r}}()).replace(/\/$/,"")}/${o.replace(/^\//,"")}`:""}export{o as t};
